dc0e8700cb53932d1ce6d4f8ccaffe8cb61d1e78
gilsaia <794433219@qq.com>
2019-11-19 15:16:11 +0800

    lab3

--- labcodes/lab3/kern/mm/vmm.c
+++ labcodes/lab3/kern/mm/vmm.c
@@ -376,7 +376,18 @@ do_pgfault(struct mm_struct *mm, uint32_t error_code, uintptr_t addr) {
         }
     }
     else{
-        
+        if(swap_init_ok)
+        {
+            struct Page *page=NULL;
+            swap_in(mm,addr,&page);
+            page_insert(mm->pgdir,page,addr,perm);
+            swap_map_swappable(mm,addr,page,1);
+            page->pra_vaddr=addr;
+        }
+        else {
+            cprintf("no swap_init_ok but ptep is %x, failed\n",*ptep);
+            goto failed;
+        }
     }
 #if 0
     /*LAB3 EXERCISE 1: YOUR CODE*/
