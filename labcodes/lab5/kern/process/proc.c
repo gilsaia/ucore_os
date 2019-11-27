72a1cd06ed5f8caef68e5875e46420fead5e37cb
gilsaia <794433219@qq.com>
2019-11-27 11:42:39 +0800

    lab5init

--- labcodes/lab5/kern/process/proc.c
+++ labcodes/lab5/kern/process/proc.c
@@ -103,6 +103,18 @@ alloc_proc(void) {
      *       uint32_t flags;                             // Process flag
      *       char name[PROC_NAME_LEN + 1];               // Process name
      */
+    proc->state=PROC_UNINIT;
+    proc->pid=-1;
+    proc->runs=0;
+    proc->kstack=0;
+    proc->need_resched=0;
+    proc->parent=NULL;
+    proc->mm=NULL;
+    memset(&(proc->context),0,sizeof(struct context));
+    proc->tf=NULL;
+    proc->cr3=boot_cr3;
+    proc->flags=0;
+    memset(proc->name,0,PROC_NAME_LEN);
      //LAB5 YOUR CODE : (update LAB4 steps)
     /*
      * below fields(add in LAB5) in proc_struct need to be initialized	
@@ -395,6 +407,25 @@ do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf) {
     //    5. insert proc_struct into hash_list && proc_list
     //    6. call wakeup_proc to make the new child process RUNNABLE
     //    7. set ret vaule using child proc's pid
+    if((proc=alloc_proc())==NULL)
+    {
+        goto fork_out;
+    }
+    if(setup_kstack(proc)!=0)
+    {
+        goto bad_fork_cleanup_proc;
+    }
+    if(copy_mm(clone_flags,proc)!=0)
+    {
+        goto bad_fork_cleanup_kstack;
+    }
+    copy_thread(proc,stack,tf);
+    proc->pid = get_pid();
+    hash_proc(proc);
+    list_add(&proc_list, &(proc->list_link));
+    nr_process ++;
+    wakeup_proc(proc);
+    ret=proc->pid;
 
 	//LAB5 YOUR CODE : (update LAB4 steps)
    /* Some Functions
